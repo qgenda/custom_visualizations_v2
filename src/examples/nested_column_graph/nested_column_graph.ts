afecae31efe706cc2fe508a523b9928deb40a779
Chris Adams <chris.adams@qgenda.com>
2019-06-10 13:04:28 -0400

    QGA-54608 more text changes

--- src/examples/nested_column_graph/nested_column_graph.ts
+++ src/examples/nested_column_graph/nested_column_graph.ts
@@ -201,9 +201,9 @@ const vis: NestedColumnGraphVisualization = {
       g.selectAll(".x-axis")
         .selectAll("g")
         .append("text")
-          .attr("x", (measureX(m.label_short) || 0))
-          .attr("y", 15)
+          .attr("transform", `translate(${measureX(m.label_short) || 0}, 15)`)
           .attr("fill", "#000")
+          .attr("text-anchor", "end")
           .attr("font-size", 8)
           .text(m.label_short);
     });
